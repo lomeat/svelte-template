a40f3831c91cb15d1fa65bd0dfb9ea70c8f1395b
lomeat <lom3at@gmail.com>
2020-05-21 03:25:43 +0300

    add default global styles (reset)

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,18 @@
 <h1>Hello, {name}</h1>
 
 <style lang="scss">
+  :global(body) {
+    margin: 0;
+    padding: 0;
+  }
+
+  :global(*) {
+    margin: 0;
+    padding: 0;
+    user-select: none;
+    outline: none;
+  }
+
   h1 {
     color: $color;
   }
